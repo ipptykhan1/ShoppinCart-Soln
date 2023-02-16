6c8d9fe5c9450ca228595928d1fa652229bdf334
ipptykhan <ipptykhancse@gmail.com>
2023-02-16 10:24:14 +0600

    added

--- index.js
+++ index.js
@@ -15,16 +15,22 @@ caseNumberField.value = newCaseNumber;
 return newCaseNumber;
 }
 
-document.getElementById('btn case plus').addEventListener('click', function(){
- const newCaseNumber = updateCaseNumber(true);
-  
+function updateCaseTotalPrice(newCaseNumber){
  const caseTotalPrice = newCaseNumber * 59;
  const caseTotalElement = document.getElementById('case-total');
  caseTotalElement.innerText = caseTotalPrice;
+
+
+}
+
+document.getElementById('btn case plus').addEventListener('click', function(){
+ const newCaseNumber = updateCaseNumber(true);
+  
+ updateCaseTotalPrice(newCaseNumber);
 })
 
 document.getElementById('btn case minus').addEventListener('click', function(){
    const newCaseNumber = updateCaseNumber(false);
 
-
+   updateCaseTotalPrice(newCaseNumber);
 })  
